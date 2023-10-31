74ea2b5bb0ffa1fa3de35f5d5eb10791a67fa919
Daria <Daria@XOF4VR>
2023-10-31 20:28:31 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace Daria_Cerneavschi_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+
+    }
+}
